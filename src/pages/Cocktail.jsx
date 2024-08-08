29d5aa22f75972b8d1fad66aa38b468176e94230
Alexis Salgado <chimalexoficial@gmail.com>
2024-08-08 13:04:46 -0600

    adding ingredients on cocktails

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -25,6 +25,10 @@ const Cocktail = () => {
     strGlass: glass,
     strInstructions: instructions } = singleDrink;
 
+  const validIngredients = Object.keys(singleDrink).filter((key) => key.startsWith('strIngredient') && singleDrink[key] !== null).map((key) => singleDrink[key]);
+
+  console.log(validIngredients);
+
 
   return (
     <Wrapper>
@@ -34,6 +38,38 @@ const Cocktail = () => {
       </header>
       <div className="drink">
         <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">              name:</span>{name}
+          </p>
+
+          <p>
+            <span className="drink-data">              category:</span>{category}
+          </p>
+
+          <p>
+            <span className="drink-data">              info:</span>{info}
+          </p>
+
+          <p>
+            <span className="drink-data">              glass:</span>{glass}
+          </p>
+
+          <p>
+            <span className="drink-data">              ingredients:</span>
+            {
+              validIngredients.map((item, index) => {
+                return <span 
+                className="ing"
+                key={index}>{item}{index < validIngredients.length - 1 ? ',':''}</span>
+              })
+            }
+          </p>
+
+          <p>
+            <span className="drink-data">              instructions:</span>{instructions}
+          </p>
+        </div>
       </div>
     </Wrapper>
   )
